56bfccfbf0402399fdb046b566efcaef15e4405c
snacksthecat <snacksthecat123@gmail.com>
2022-08-19 17:56:29 -0500

    NUBS

--- keyboards/converter/ibmpc_usb/keymaps/default/keymap.c
+++ keyboards/converter/ibmpc_usb/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RESET,   KC_ESC,   KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NUHS, KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, KC_NLCK,      KC_PSLS, KC_PAST, KC_PMNS,
         KC_VOLD, KC_VOLU,  KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, KC_P7,        KC_P8,   KC_P9,   KC_PPLS,
         KC_MPLY, KC_MSTP,  KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,          KC_ENT,           KC_UP,            KC_P4,        KC_P5,   KC_P6,   KC_PCMM,
-        KC_MPRV, KC_MNXT,  KC_LSFT, KC_NUHS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_NO,            KC_RSFT, KC_LEFT, KC_APP,  KC_RGHT, KC_P1,        KC_P2,   KC_P3,   KC_PENT,
+        KC_MPRV, KC_MNXT,  KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_NO,            KC_RSFT, KC_LEFT, KC_APP,  KC_RGHT, KC_P1,        KC_P2,   KC_P3,   KC_PENT,
         KC_LGUI, KC_PSCR,  KC_LCTL,          KC_LALT,                            KC_SPC,                             KC_RALT,                   KC_RCTL,                            KC_DOWN,          KC_RGUI,      KC_P0,   KC_PDOT, KC_NO
     ),
     [1] = LAYOUT(

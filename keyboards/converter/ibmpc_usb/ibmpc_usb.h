143a4f6da294602e1f1a8f138f31490cd49dc623
snacksthecat <snacksthecat123@gmail.com>
2022-08-23 22:07:35 -0500

    removed nuhs

--- keyboards/converter/ibmpc_usb/ibmpc_usb.h
+++ keyboards/converter/ibmpc_usb/ibmpc_usb.h
@@ -35,7 +35,7 @@ void matrix_init_user(void);
     K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, K3F, K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, \
     K4A, K4B, K4C, K4D, K4E, K4F, K50, K51, K52, K53, K54, K55, K42, K56, K57, K58, K59, K5A, \
     K5B, K5C, K5D, K5E, K5F, K60, K61, K62, K63, K64, K65, K66, K67, K68, K69, K6A, K6B, K6C, K6D, \
-    K6E, K6F, K70, K71, K72, K73, K74, K75, K76, K77, K78, K79, K7A, K7B, K7C, K7D, K7E, K7F \
+    K6E, K6F, K70, K71, K72, K73, K74, K75, K76, K77, K78, K79, K7A, K7B,  K7D, K7E, K7F \
 ) { \
  { K00, K01, K02, K03, K04, K05, K06, K07,  \
   K08, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
@@ -47,7 +47,7 @@ void matrix_init_user(void);
   K38, K39, K3A, K3B, K3C, K3D, K3E, K3F }, \
  { K40, K41, K42, K43, K44, K45, K46, K47,  \
   K48, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
- { K50, K51, K52, K53, K54, K55, K42, K56, K57,  \
+ { K50, K51, K52, K53, K54, K55, K56, K57,  \
   K58, K59, K5A, K5B, K5C, K5D, K5E, K5F }, \
  { K60, K61, K62, K63, K64, K65, K66, K67,  \
   K68, K69, K6A, K6B, K6C, K6D, K6E, K6F }, \

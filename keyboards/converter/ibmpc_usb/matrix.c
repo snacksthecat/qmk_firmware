bcbc9c031944521c2b197ab228f1a12c11fa1638
snacksthecat <snacksthecat123@gmail.com>
2022-08-27 23:49:19 -0500

    Uncommented LED section

--- keyboards/converter/ibmpc_usb/matrix.c
+++ keyboards/converter/ibmpc_usb/matrix.c
@@ -527,27 +527,27 @@ bool matrix_has_ghost_in_row(uint8_t row)
 void led_set(uint8_t usb_led)
 {
     uint8_t ibmpc_led = 0;
-//    if (usb_led &  (1<<USB_LED_SCROLL_LOCK)) {
-//        DDRF |= (1<<7);
-//        PORTF |= (1<<7);
-//    } else {
-//        DDRF &= ~(1<<7);
-//        PORTF &= ~(1<<7);
-//    }
-//    if (usb_led &  (1<<USB_LED_NUM_LOCK)) {
-//        DDRF |= (1<<6);
-//        PORTF |= (1<<6);
-//    } else {
-//        DDRF &= ~(1<<6);
-//        PORTF &= ~(1<<6);
-//    }
-//    if (usb_led &  (1<<USB_LED_CAPS_LOCK)) {
-//        DDRF |= (1<<5);
-//        PORTF |= (1<<5);
-//    } else {
-//        DDRF &= ~(1<<5);
-//        PORTF &= ~(1<<5);
-//    }
+   if (usb_led &  (1<<USB_LED_SCROLL_LOCK)) {
+       DDRF |= (1<<7);
+       PORTF |= (1<<7);
+   } else {
+       DDRF &= ~(1<<7);
+       PORTF &= ~(1<<7);
+   }
+   if (usb_led &  (1<<USB_LED_NUM_LOCK)) {
+       DDRF |= (1<<6);
+       PORTF |= (1<<6);
+   } else {
+       DDRF &= ~(1<<6);
+       PORTF &= ~(1<<6);
+   }
+   if (usb_led &  (1<<USB_LED_CAPS_LOCK)) {
+       DDRF |= (1<<5);
+       PORTF |= (1<<5);
+   } else {
+       DDRF &= ~(1<<5);
+       PORTF &= ~(1<<5);
+   }
     // Sending before keyboard recognition may be harmful for XT keyboard
     if (keyboard_kind == NONE) return;
 
